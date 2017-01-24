481dd3df6c601e8693d04fa21a66e14405760ee4
Raphael Thiney <raphael@surgipal.com>
2017-01-24 22:42:16 +0000

    Updated deploy.cmd

--- deploy.cmd
+++ deploy.cmd
@@ -88,15 +88,7 @@ goto :EOF
 :Deployment
 echo Handling node.js deployment.
 
-:: 1. KuduSync
-:: IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
- :: call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%/dist" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
-::  IF !ERRORLEVEL! NEQ 0 goto error
-::)
-
-:: 2. Select node version
-call :SelectNodeVersion
-
+ 
 :: 3. Install npm packages
 IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   pushd "%DEPLOYMENT_TARGET%"
